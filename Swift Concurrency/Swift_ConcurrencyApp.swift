c47ed36de72bf986e8234f73886561c8e93e4ff3
Nizami Tagiyev <nizamion24s@gmail.com>
2022-05-31 11:15:45 +0500

    Initial Commit

---
+++ Swift Concurrency/Swift_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ConcurrencyApp.swift
+//  Swift Concurrency
+//
+//  Created by Nizami Tagiyev on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
