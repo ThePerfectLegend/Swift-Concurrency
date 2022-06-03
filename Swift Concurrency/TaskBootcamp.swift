134c69598bd394de25c855439663f3a09ed517f8
Nizami Tagiyev <nizamion24s@gmail.com>
2022-06-03 18:14:10 +0500

    How to use Task and .task in Swift

---
+++ Swift Concurrency/TaskBootcamp.swift
@@ -0,0 +1,87 @@
+//
+//  TaskBootcamp.swift
+//  Swift Concurrency
+//
+//  Created by Nizami Tagiyev on 03.06.2022.
+//
+
+import SwiftUI
+
+class TaskBootcampViewModel: ObservableObject {
+    
+    @Published var image: UIImage?
+    @Published var image2: UIImage?
+    
+    func fetchImage() async {
+        try? await Task.sleep(nanoseconds: 5_000_000_000)
+        do {
+            guard let url = URL(string: "https://picsum.photos/1000") else { return }
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            await MainActor.run(body: {
+                self.image = UIImage(data: data)
+                print("Image returned successfully")
+            })
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func fetchImage2() async {
+        do {
+            guard let url = URL(string: "https://picsum.photos/1000") else { return }
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            self.image2 = UIImage(data: data)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
+
+struct TaskBootcampHomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                NavigationLink("Click Me!") {
+                    TaskBootcamp()
+                }
+            }
+        }
+    }
+}
+
+struct TaskBootcamp: View {
+    
+    @StateObject private var viewModel = TaskBootcampViewModel()
+    @State private var fetchImageTask: Task<(), Never>?
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            
+            if let image2 = viewModel.image {
+                Image(uiImage: image2)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+        }
+        .task {
+            await viewModel.fetchImage()
+        }
+//        .onDisappear {
+//            fetchImageTask?.cancel()
+//        }
+//        .onAppear {
+//            fetchImageTask = Task {
+//                await viewModel.fetchImage()
+//            }
+//        }
+    }
+}
+
