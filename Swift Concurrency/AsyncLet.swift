77b502e9cb5876bd846ba29caae1dc81a4073fc9
Nizami Tagiyev <nizamion24s@gmail.com>
2022-06-05 16:05:45 +0500

    use Async Let to perform concurrent methods in Swift

---
+++ Swift Concurrency/AsyncLet.swift
@@ -0,0 +1,83 @@
+//
+//  AsyncLet.swift
+//  Swift Concurrency
+//
+//  Created by Nizami Tagiyev on 05.06.2022.
+
+// One of the most convenient additions to async / await is async-let. This allows us to easily perform multiple asynchronous functions at the same time.
+
+import SwiftUI
+
+struct AsyncLet: View {
+    
+    @State var images: [UIImage] = []
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    let url = URL(string: "https://picsum.photos/300")!
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                    }
+                }
+            }
+            .navigationTitle("Async Let")
+            .onAppear {
+                Task {
+                    do {
+                        
+                        async let fetchImage1 = fetchImage()
+                        async let fetchTitle1 = fetchTitle()
+                        async let fetchImage2 = fetchImage()
+                        async let fetchImage3 = fetchImage()
+                        async let fetchImage4 = fetchImage()
+                        
+                        let (image1, image2, image3, image4) = await (try fetchImage1, try fetchImage2, try fetchImage3, try fetchImage4)
+                        self.images.append(contentsOf: [image1, image2, image3, image4])
+                        
+                        let title1 = try await fetchTitle1
+                        
+//                        let image1 = try await fetchImage()
+//                        self.images.append(image1)
+//
+//                        let image2 = try await fetchImage()
+//                        self.images.append(image2)
+//
+//                        let image3 = try await fetchImage()
+//                        self.images.append(image3)
+//
+//                        let image4 = try await fetchImage()
+//                        self.images.append(image4)
+                        
+                    } catch {
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    func fetchImage() async throws -> UIImage {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            if let image = UIImage(data: data) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
+        }
+    }
+    
+    func fetchTitle() async -> String {
+        return "New Title"
+    }
+    
+}
+
